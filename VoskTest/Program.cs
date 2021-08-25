1318a43a6eb02403aa083d78824e9b4728cbd69e
LordKmon <demonkmon@gmail.com>
2021-08-25 17:58:59 +0300

    Add "red.wav" file
    Add "red.wav" file,
    trying recongize it by DemoFloats

--- VoskTest/Program.cs
+++ VoskTest/Program.cs
@@ -12,11 +12,11 @@ namespace VoskTest
     {
         static void Main(string[] args)
         {
-            string path_eng = "E:\\Artem\\Vosk\\Wave\\red_five.wav";
+            string path_eng = "E:\\Artem\\Vosk\\Wave\\red.wav";
             string model_eng = "E:\\Artem\\Vosk\\Models\\vosk-model-en-us-0.20";
 
             Vosk.Model model = new Model(model_eng);
-            DemoBytes(model, path_eng);
+            DemoFloats(model, path_eng);
             Console.ReadKey();
         }
 
@@ -43,9 +43,34 @@ namespace VoskTest
                 }
             }
 
-            string result = rec.Result();
-            string final_result = rec.FinalResult();
-            string win = "yes";
+            Console.WriteLine(rec.FinalResult());
+        }
+
+        public static void DemoFloats(Model model, string a_FileName)
+        {
+            // Demo float array
+            VoskRecognizer rec = new VoskRecognizer(model, 16000.0f);
+            using (Stream source = File.OpenRead(a_FileName))
+            {
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    float[] fbuffer = new float[bytesRead / 2];
+                    for (int i = 0, n = 0; i < fbuffer.Length; i++, n += 2)
+                    {
+                        fbuffer[i] = (short)(buffer[n] | buffer[n + 1] << 8);
+                    }
+                    if (rec.AcceptWaveform(fbuffer, fbuffer.Length))
+                    {
+                        Console.WriteLine(rec.Result());
+                    }
+                    else
+                    {
+                        Console.WriteLine(rec.PartialResult());
+                    }
+                }
+            }
             Console.WriteLine(rec.FinalResult());
         }
     }
